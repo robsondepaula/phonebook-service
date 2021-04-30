f55ff9e818acce0bdd20715e2efe37460cef3319
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-04-30 16:56:53 -0300

    Finished working on 3.2.

--- index.js
+++ index.js
@@ -25,8 +25,32 @@ let persons = [{
     }
 ]
 
+// weekday month day year time GMT
+const getDate = () => {
+    const weekNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const d = new Date();
+    const weekName = weekNames[d.getDay()];
+    const monthName = monthNames[d.getMonth()]
+    const day = d.getDate()
+    const year = d.getFullYear()
+    const time = d.toTimeString()
+
+    return `${weekName} ${monthName} ${day} ${year} ${time}`
+}
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+    const length = persons ? persons.length : 0
+    const returnData = length > 0 ? 'Phonebook has information on ' + persons.length + ' people\n' : ''
+
+    response.send(returnData.concat(getDate()))
 })
 
 const PORT = 3001
