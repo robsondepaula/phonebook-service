85670a05fd6ace361eab82c40a5fd4f2c9b881f6
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-01 16:52:37 -0300

    Finished working on 3.4.

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    person = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
