e3cf74930dfe169a559622a646382b8c2b932fe8
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-02 10:40:48 -0300

    Finished exercise 3.11.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('phonebook', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :phonebook'))
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 let persons = [{
