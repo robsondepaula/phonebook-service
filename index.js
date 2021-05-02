fc22e013dd8c70faf209c396ee3ad4817b9513a7
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-02 10:25:38 -0300

    Working on exercise 3.9.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ morgan.token('phonebook', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :phonebook'))
 app.use(express.json())
+app.use(cors())
 
 let persons = [{
         "id": 1,
@@ -113,7 +115,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
