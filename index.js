2abd6520dec601f9bed66f708d466ba92bb87537
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-05-08 14:32:25 -0300

    Finished exercises up to 3.21.

--- index.js
+++ index.js
@@ -67,49 +67,27 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if ((!body.name) || (!body.number)) {
-        return response.status(400).json({
-            error: 'invalid input'
-        })
-    }
-
-    Person.find({}).then(persons => {
-        let person = persons.find(person => person.name === body.name)
-        if (person) {
-            Person.findByIdAndUpdate(person.id, person, {
-                    number: body.number
-                })
-                .then(updatedPerson => {
-                    response.json(updatedPerson)
-                })
-                .catch(error => next(error))
-        } else {
-            person = new Person({
-                name: body.name,
-                number: body.number
-            })
-
-            person.save()
-                .then(savedPerson => {
-                    response.json(savedPerson)
-                })
-                .catch(error => next(error))
-        }
+    person = new Person({
+        name: body.name,
+        number: body.number
     })
+
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
-    if ((!body.name) || (!body.number)) {
-        return response.status(400).json({
-            error: 'invalid input'
-        })
+    const opts = {
+        runValidators: true
     }
-
     Person.findByIdAndUpdate(request.params.id, {
             number: body.number
-        })
+        }, opts)
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -122,6 +100,10 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({
             error: 'malformatted id'
+        })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({
+            error: error.message
         })
     }
 
